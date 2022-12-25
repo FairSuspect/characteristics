c41190f0de160da170aef999e309e7fc234c1d9d
Kirill Velikanov <velik.kir@gmail.com>
2022-12-25 20:08:55 +0300

    Report

--- lib/characteristics.dart
+++ lib/characteristics.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:intl/intl.dart';
+
 int calculate() {
   return 6 * 7;
 }
@@ -10,7 +12,8 @@ Future<void> saveHtml(List<String> characteristics) async {
   for (var characteristic in characteristics) {
     html += "<p>$characteristic</p>";
   }
-
+  html +=
+      "<p>Дата созадния документа ${DateFormat.yMd().format(DateTime.now())}</p>";
   html += "</body></html>";
 
 // Сохраняем файл
